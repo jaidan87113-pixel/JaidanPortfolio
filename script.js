3118a8ecb7c7442c8705ea885e0715b18ba54eb5
JswizL <jaidan87113@gmail.com>
2026-03-17 17:59:32 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Cursor effect
+document.addEventListener('DOMContentLoaded', function() {
+    const cursor = document.createElement('div');
+    cursor.classList.add('custom-cursor');
+    document.body.appendChild(cursor);
+
+    document.addEventListener('mousemove', function(e) {
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+    });
+});
